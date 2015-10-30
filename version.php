bead11e4a923575c009868e5662775781f16212a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-10-29 21:27:32 -0700

    bumped version number.

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
  *
  */
 
-$plugin->version   = 2016062000;
+$plugin->version   = 2016062001;
 $plugin->requires  = 2010112400;  //YYYYMMDDHH (This is the release version of Moodle 2.0)
 $plugin->component = 'block_ilios';
